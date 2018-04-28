c4e484ccfe4d0bf18036d245212e48e440db17dc
yong.zhao <yong.zhao@goland.cn>
2018-04-28 11:21:40 +0800

    init graphics view

--- QtTestProject/widget.cpp
+++ QtTestProject/widget.cpp
@@ -6,9 +6,22 @@ Widget::Widget(QWidget *parent) :
     ui(new Ui::Widget)
 {
     ui->setupUi(this);
+
+    InitGView();
 }
 
 Widget::~Widget()
 {
     delete ui;
+}
+
+void Widget::InitGView()
+{
+    QGraphicsScene *nScene = new QGraphicsScene(this);
+    ui->gVw->setScene(nScene);
+
+    ui->gVw->setWindowTitle("test");
+    ui->gVw->resize(200, 200);
+
+    nScene->addLine(0, 50, 100, 150);
 }
--- QtTestProject/widget.h
+++ QtTestProject/widget.h
@@ -15,6 +15,8 @@ public:
     explicit Widget(QWidget *parent = 0);
     ~Widget();
 
+    void InitGView(void);
+
 private:
     Ui::Widget *ui;
 };
